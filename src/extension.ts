bb6479ee2f79ca28de9f962841e88ebc26674dbf
Nima Karami <karami.nima@gmail.com>
2026-06-08 23:07:49 -0400

    feat: wire registry/host/manager + openDashboard command

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,24 @@
 import * as vscode from 'vscode';
+import { AgentRegistry } from './agentRegistry';
+import { VsCodeTerminalHost } from './terminalHost';
+import { SessionManager } from './sessionManager';
+import { DashboardPanel } from './dashboardPanel';
+import { AgentDefinition } from './types';
 
-export function activate(_context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+  const defs = vscode.workspace
+    .getConfiguration('agentDeck')
+    .get<AgentDefinition[]>('agents', []);
+  const registry = new AgentRegistry(defs);
+  const host = new VsCodeTerminalHost();
+  const manager = new SessionManager(registry, host);
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('agentDeck.openDashboard', () =>
+      DashboardPanel.show(context, manager, registry),
+    ),
+    { dispose: () => host.cleanup() },
+  );
+}
 
 export function deactivate() {}
